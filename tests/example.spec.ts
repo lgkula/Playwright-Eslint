6e9c3222dd57f61f817362da225e799778af11ec
Leslaw Kula <Leslaw.Kula@kruksa.pl>
2024-11-02 01:05:51 +0100

    update test

--- tests/example.spec.ts
+++ tests/example.spec.ts
@@ -7,12 +7,19 @@ test('has title', async ({ page }) => {
   await expect(page).toHaveTitle(/Playwright/);
 });
 
-test('get started link', async ({ page }) => {
-  await page.goto('https://playwright.dev/');
+test.skip("get started link", async ({ page }) => {
+  await page.goto("https://playwright.dev/");
 
   // Click the get started link.
-  await page.getByRole('link', { name: 'Get started' }).click();
+  await page.getByRole("link", { name: "Get started" }).click();
 
   // Expects page to have a heading with the name of Installation.
-  await expect(page.getByRole('heading', { name: 'Installation' })).toBeVisible();
+  await expect(
+    page.getByRole("heading", { name: "Installation" })
+  ).toBeVisible();
+});
+
+test("Not used variables test", async ({ page }) => {
+  let a = 1;
+  const b = 2;
 });
